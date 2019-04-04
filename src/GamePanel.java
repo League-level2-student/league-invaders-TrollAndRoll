0cd5afd26fa4d7de74fba6bbfc178080028488e9
Joel Castro <Joel Castro@DESKTOP-OM0LJCA.mshome.net>
2019-04-03 22:00:01 -0700

    LeagueInvaders Part 5 Finished

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,14 +9,17 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer;
-
+	GameObject gameObject;
+	
 	public GamePanel() {
 		timer = new Timer(1000/60, this);
+		gameObject = new GameObject(0, 0, 0, 0);
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		gameObject.update();
 		repaint();
 	}
 	
@@ -29,22 +32,36 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	public void paintComponent(Graphics g){
 		g.fillRect(10, 10, 100, 100);
+		gameObject.draw(g);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("key pressed");
+		int keyCode = e.getKeyCode();
+		if(keyCode == KeyEvent.VK_W) {
+			gameObject.y = gameObject.y - 10;
+		}
+		else if(keyCode == KeyEvent.VK_A) {
+			gameObject.x = gameObject.x - 10;
+		}
+		else if(keyCode == KeyEvent.VK_S) {
+			gameObject.y = gameObject.y + 10;
+		}
+		else if(keyCode == KeyEvent.VK_D) {
+			gameObject.x = gameObject.x + 10;
+		}
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("key released");
 	}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("key typed");
 	}
