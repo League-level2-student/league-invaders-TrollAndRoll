2bf94573e3601c8667e9547bf32092f3e6a9c3a8
329525 <329525@0338S-S-R0B6ZHR.sdcs.local>
2019-05-28 14:26:10 -0700

    Leauge Invaders Pt.10 step 12 IP

--- src/GameObject.java
+++ src/GameObject.java
@@ -6,6 +6,8 @@ public class GameObject{
     int y;
     int width;
     int height;
+    
+    boolean isAlive = true;
 	 
 	public GameObject(int x, int y, int width, int height) {
 		this.x = x;
--- src/objectManager.java
+++ src/objectManager.java
@@ -1,4 +1,18 @@
+import java.awt.Graphics;
 
 public class objectManager {
 
+	Rocketship ship;
+	
+	public objectManager(Rocketship rocket){
+		this.ship = rocket;
+	}
+	
+	void update() {
+		ship.update();
+	}
+	
+	void draw(Graphics g) {
+		ship.draw(g);
+	}
 }
